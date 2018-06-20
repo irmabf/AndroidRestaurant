f4908d747714cce94319f279437ed75e759a9118
Irma <irblancofdz@gmail.com>
2018-06-20 04:54:33 +0200

    Fix some issues with RecyclerView

--- app/src/main/java/com/irmabf/androidrestaurant/model/Tables.kt
+++ app/src/main/java/com/irmabf/androidrestaurant/model/Tables.kt
@@ -7,29 +7,60 @@ object Tables{
     private val tables: List<Table> = listOf(
             Table(
                     "Mesa 1", listOf(
-                            Dish("Donut", R.drawable.donut, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                            Dish("Donut", R.drawable.donut, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                            Dish("Donut", R.drawable.donut, 5f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                            Dish("Donut", R.drawable.donut, 6f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                            Dish("Donut", R.drawable.donut, 7f, "Donut Relleno de Crema", listOf("1, 2, 3") )
+                        Dish("Salmon a la parrilla", R.drawable.salmon_dish, 5f, "Salmon fresco a la parrilla", listOf("1")),
+                        Dish("Hamburguesa con Patatas", R.drawable.burger_fries_dish, 3f, "Hamburguesa de pollo o vacuno con patatas fritas", listOf("1", "2")),
+                        Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Pasta", R.drawable.pasta_dish, 4f, "Spaguetti Carbonara", listOf("1, 2, 3") ),
+                        Dish("Arroz", R.drawable.arroz_delicias, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Donut", R.drawable.donut, 3f, "Donut Relleno de Crema", listOf("1", "2", "3") ),
+                        Dish("Café", R.drawable.coffee, .75f, "Café con leche o café solo", listOf("1")),
+                        Dish("Cerveza", R.drawable.cerveza_jarra, 1.5f, "Jarra de cerveza fresca", listOf("1") )
                     )),
             Table(
                     "Mesa 2", listOf(
-                    Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 5f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 6f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 7f, "Donut Relleno de Crema", listOf("1, 2, 3") )
-                )),
+                        Dish("Salmon a la parrilla", R.drawable.salmon_dish, 5f, "Salmon fresco a la parrilla", listOf("1")),
+                        Dish("Hamburguesa con Patatas", R.drawable.burger_fries_dish, 3f, "Hamburguesa de pollo o vacuno con patatas fritas", listOf("1", "2")),
+                        Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Pasta", R.drawable.pasta_dish, 4f, "Spaguetti Carbonara", listOf("1, 2, 3") ),
+                        Dish("Arroz", R.drawable.arroz_delicias, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Donut", R.drawable.donut, 3f, "Donut Relleno de Crema", listOf("1", "2", "3") ),
+                        Dish("Café", R.drawable.coffee, .75f, "Café con leche o café solo", listOf("1")),
+                        Dish("Cerveza", R.drawable.cerveza_jarra, 1.5f, "Jarra de cerveza fresca", listOf("1") )
+                    )),
 
             Table(
-                    "Mesa 1", listOf(
-                    Dish("Arroz", R.drawable.arroz_delicias, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Arroz", R.drawable.arroz_delicias, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Arroz", R.drawable.arroz_delicias, 5f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Arroz", R.drawable.arroz_delicias, 6f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
-                    Dish("Arroz", R.drawable.arroz_delicias, 7f, "Donut Relleno de Crema", listOf("1, 2, 3") )
-                ))
+                    "Mesa 3", listOf(
+                        Dish("Salmon a la parrilla", R.drawable.salmon_dish, 5f, "Salmon fresco a la parrilla", listOf("1")),
+                        Dish("Hamburguesa con Patatas", R.drawable.burger_fries_dish, 3f, "Hamburguesa de pollo o vacuno con patatas fritas", listOf("1", "2")),
+                        Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Pasta", R.drawable.pasta_dish, 4f, "Spaguetti Carbonara", listOf("1, 2, 3") ),
+                        Dish("Arroz", R.drawable.arroz_delicias, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Donut", R.drawable.donut, 3f, "Donut Relleno de Crema", listOf("1", "2", "3") ),
+                        Dish("Café", R.drawable.coffee, .75f, "Café con leche o café solo", listOf("1")),
+                        Dish("Cerveza", R.drawable.cerveza_jarra, 1.5f, "Jarra de cerveza fresca", listOf("1") )
+                )),
+            Table(
+                    "Mesa 4", listOf(
+                        Dish("Salmon a la parrilla", R.drawable.salmon_dish, 5f, "Salmon fresco a la parrilla", listOf("1")),
+                        Dish("Hamburguesa con Patatas", R.drawable.burger_fries_dish, 3f, "Hamburguesa de pollo o vacuno con patatas fritas", listOf("1", "2")),
+                        Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Pasta", R.drawable.pasta_dish, 4f, "Spaguetti Carbonara", listOf("1, 2, 3") ),
+                        Dish("Arroz", R.drawable.arroz_delicias, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Donut", R.drawable.donut, 3f, "Donut Relleno de Crema", listOf("1", "2", "3") ),
+                        Dish("Café", R.drawable.coffee, .75f, "Café con leche o café solo", listOf("1")),
+                        Dish("Cerveza", R.drawable.cerveza_jarra, 1.5f, "Jarra de cerveza fresca", listOf("1") )
+            )),
+            Table(
+                    "Mesa 5", listOf(
+                        Dish("Salmon a la parrilla", R.drawable.salmon_dish, 5f, "Salmon fresco a la parrilla", listOf("1")),
+                        Dish("Hamburguesa con Patatas", R.drawable.burger_fries_dish, 3f, "Hamburguesa de pollo o vacuno con patatas fritas", listOf("1", "2")),
+                        Dish("Pizza Queso", R.drawable.pizza_quesos_plato, 4f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Pasta", R.drawable.pasta_dish, 4f, "Spaguetti Carbonara", listOf("1, 2, 3") ),
+                        Dish("Arroz", R.drawable.arroz_delicias, 3f, "Donut Relleno de Crema", listOf("1, 2, 3") ),
+                        Dish("Donut", R.drawable.donut, 3f, "Donut Relleno de Crema", listOf("1", "2", "3") ),
+                        Dish("Café", R.drawable.coffee, .75f, "Café con leche o café solo", listOf("1")),
+                        Dish("Cerveza", R.drawable.cerveza_jarra, 1.5f, "Jarra de cerveza fresca", listOf("1") )
+            ))
 
     )
     val count
